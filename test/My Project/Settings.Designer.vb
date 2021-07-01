1467c1e7156fefdef6f4277e7e4d381830bd78b1
Ameer Shah <ameer@bayam.my>
2021-07-01 18:12:10 +0800

    rename

--- test/My Project/Settings.Designer.vb
+++ test/My Project/Settings.Designer.vb
@@ -75,9 +75,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.test.My.MySettings
+        Friend ReadOnly Property Settings() As Global.crud.My.MySettings
             Get
-                Return Global.test.My.MySettings.Default
+                Return Global.crud.My.MySettings.Default
             End Get
         End Property
     End Module
